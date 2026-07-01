c9c897d7378a551ca904a50030beab7400a5ae36
Lucas Eduardo <lucaseduardo12042005@gmail.com>
2026-07-01 11:30:09 -0300

    inicio da lógica operacional

--- script.js
+++ script.js
@@ -17,9 +17,13 @@
 // const valorDaAcao = botaoLimpar.dataset.acao;
 
 
+// *********************************VARIÁVEIS*************************************** 
+// ********************************************************************************* 
+
 //pego o visor e armazeno ele dentro de uma variável
 const visor = document.querySelector("#visor")
-
+const igual = document.querySelector("#resultado")
+exibirNoVisor("") 
 
 //Aqui eu crio 3 listas, todas com seus respectivos valores dentro uma para botões de 
 //ações, uma para as teclas numeradas, e uma para os operadores
@@ -27,13 +31,20 @@ const botoesAcao = document.querySelectorAll("[data-acao]")
 const teclas = document.querySelectorAll("[data-numero]")
 const operacao = document.querySelectorAll("[data-operador]")
 
+
 //Defino uma variável, não constante para montar a expressão
 let expressao = ""
+let expressaodividida;
 
-//verificação, caso tudo ocorreu bem, este deve exibir as listas com valores
+//verificação, se deu certo, vai exibir as listas com valores
 console.log(botoesAcao)
 console.log(teclas)
 console.log(operacao)
+
+
+// *********************************EVENTOS*************************************** 
+// ********************************************************************************* 
+
 
 botoesAcao.forEach(function(acao){ // Dentro de botoesAcoes (lista), o forEach vai       
                                   // percorrer todos os itens
@@ -49,6 +60,10 @@ botoesAcao.forEach(function(acao){ // Dentro de botoesAcoes (lista), o forEach v
 
     if(botaoClicado.innerText == "AC"){ //se o texto do botão clicado for AC
       limpar()                          // é limpa a expressão e o visor
+    } else if(botaoClicado.innerText == "+/-"){
+      console.log("clicou +/-")
+    }else if(botaoClicado.innerText == "%"){
+      console.log("Clicou porcentagem")
     }
 
   })
@@ -73,16 +88,103 @@ teclas.forEach(function(tecla){ // Dentro de teclas (lista), o forEach vai
 operacao.forEach(function(operador){
   operador.addEventListener('click', function(e){
     const operadorSelecionado = e.target;
-    console.log(expressao)
-
+  
     expressao += operadorSelecionado.innerText;
     visor.value += operadorSelecionado.innerText;
+    console.log(expressao)
   })
 })
 
 console.log(expressao)
 
+// Até o momento, capturei os eventos e os coloquei na variável expressão agora 
+// preciso tratar a expressão, 
+// primeiro passo, isolar primeiro número, operador  e segundo número
+//REGEx
+// DESCREVENDO OS NÚMEROS
+//: número, um dígito sozinho é representado por \d (0 a 9), se haver a possibilidade
+//de ter-se mais caracteres, deve-ser inserir o + após o \d (\d+)
+//\.? Para números decimais, incluimos o (.), para caso seja opcional, incluimos o (?)]
+// \d* → dígitos depois do ponto, mas agora com * em vez de +, porque depois do ponto pode não ter nenhum dígito (ou pode ter vários)
+//FINAL: \d\.?\d*
+// DESCEVER OS OPERADORES
+// Para representar qualquer um dos possíveis operadores, o regex utiliza colchetes []
+// que representa uma classe de caracteres (qualquer um dentre os colchetes)
+//[+\-*/]
+// JUNTAR TODOS
+// Achar um número ou achar um operador: \d\.?\d* | [+\-*/]
+// //g para percorrer a string inteira e pegar todas as ocorrências
+// FINAL: /\d\.?\d*|[+\-*/]/g
+
+
+// *********************************CONVERSÃO E REGRAS*************************************** 
+// ******************************************************************************************
+
+
+igual.addEventListener('click', function(e){
+  expressaodividida = expressao.match(/\d\.?\d*|[+\-x/]/g) //Quando clicar em = expressão
+                                                          //dividida recebe array dos elementos 
+
+  primeiroNumero = parseFloat(expressaodividida[0])
+  operador = expressaodividida[1]
+  segundoNumero = parseFloat(expressaodividida[2])
+
+  if(operador == "+"){
+    somaTotal = somar(primeiroNumero, segundoNumero)
+    exibirNoVisor(somaTotal)
+  }else if(operador == "-"){
+    subtracao = subtrair(primeiroNumero, segundoNumero)
+    exibirNoVisor(subtracao)
+  }else if(operador == "x"){
+    multiplicado = multiplicar(primeiroNumero, segundoNumero)
+    exibirNoVisor(multiplicado)
+  }else if(operador == "/"){
+    dividido = dividir(primeiroNumero, segundoNumero)
+    exibirNoVisor(dividido)
+  }
+  
+})
+
+/* 
+PROBLEMAS IDENTIFICADOS ATÉ O MOMENTO
+- Numeros decimais não formatado até 2 casas decimais após o ponto
+- Não aproveitamento do ultimo resultado pra nova operação
+- Permitindo incluir sinais multiplos, como ++ XX, não se limita a um por operação
+- se inserido 3 números, quebra
+*/
+
+
+console.log(expressaodividida) 
+console.log(typeof(expressaodividida))
+
+
+// FUNÇÕES
+
+function exibirNoVisor(data){
+  visor.value = data
+}
+
 function limpar(){ //reseta a expressão e o visor
-  expressao = ""
-  visor.value = ""
+  expressao = "";
+  visor.value = "";
+}
+
+function somar(a, b){
+  soma = a + b;
+  return soma
+}
+
+function subtrair(a, b){
+  subtracao = a - b;
+  return subtracao
+}
+
+function multiplicar(a, b){
+  multiplicacao =  a * b;
+  return multiplicacao
+}
+
+function dividir(a, b){
+  divisao = a / b
+  return divisao
 }
